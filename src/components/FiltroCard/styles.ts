e80b77e55e5311cf048b1e20348f83d7dfac8158
natalete <natsalete14@gmail.com>
2024-04-28 19:48:36 -0300

    feat:construção do componente de filtro

---
+++ src/components/FiltroCard/styles.ts
@@ -0,0 +1,21 @@
+import sytled, { styled } from "styled-components";
+
+import { Props } from '.'
+
+export const Card = styled.div<Props>`
+    padding: 8px;
+    border: 1px solid ${props => props.ativo ? '#1E90FF' : '#A1A1A1'} ;
+    background-color: ${props => props.ativo ? '#FFF' : '#FCFCFC'} ;
+    color:  ${props => props.ativo ? '#1E90FF' : '#5E5E5E'} ;
+    border-radius: 8px;
+`
+
+export const Contador = styled.span`
+    font-weight: bold;
+    font-size: 24px;
+    display: block;
+`
+
+export const Label = styled.span`
+    font-size: 14px;
+`
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,7 +1,21 @@
+import FiltroCard from "../../components/FiltroCard"
+
+import * as S from './styles'
+
 const BarraLateral = () => (
-    <aside>
-        barra lateral
-    </aside>
+    <S.Aside>
+        <div>
+            <S.Campo type="text" placeholder="Busca" />
+            <S.Filtros>
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard  ativo/>
+            </S.Filtros>
+        </div>
+    </S.Aside>
 )
 
 export default BarraLateral
