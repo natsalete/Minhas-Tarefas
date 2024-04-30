1856c32f2baf3def383be9bf801ad41a37936f12
natalete <natsalete14@gmail.com>
2024-04-30 18:28:14 -0300

    feat: configuração do redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import BarraLateral from "./containers/BarraLateral";
 import ListaDeTarefas from "./containers/ListaDeTarefas";
 import EstiloGlobal, { Container } from "./styles";
 
+import  store from './store'
+import { Provider } from "react-redux";
+
 function App() {
   return (
-    <>
+    <Provider store ={store}>
       <EstiloGlobal />
       <Container>
         <BarraLateral />
         <ListaDeTarefas />
       </Container>
-    </>  
+    </Provider>
   )
 }
 
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,47 +1,34 @@
+import { useSelector } from 'react-redux'
+
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
-import * as enums from '../../utils/enums/Tarefa'
 
-const tarefas = [
-  {
-    titulo: 'Estudar  TypeScript',
-    descricao: 'Ver a aula 3 da EBAC',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  },
-  {
-    titulo: 'Pagar a conta de internet',
-    descricao: 'Baixar fatura no Gmail',
-    prioridade:enums.Prioridade.URGENTE,
-    status: enums.Status.CONCLUIDA
-  },
-  {
-    titulo: 'Ir para a academia',
-    descricao: 'Fazer treino B',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  },
-]
+import { RootReducer } from '../../store'
 
-const ListaDeTarefas = () => (
+
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) =>  state)
+
+  return(
     <Container>        
-        <p>
-            2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
-        </p>
-        <ul>
-          {tarefas.map((t) => (
-            <li key={t.titulo}>
-              <Tarefa 
-              descricao={t.descricao} 
-              titulo={t.titulo} 
-              status={t.status} 
-              prioridade={t.prioridade}
-              />
-            </li>
-          ))}
-        </ul>
-        </Container>
-
-)
+      <p>
+        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+      </p>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa 
+            descricao={t.descricao} 
+            titulo={t.titulo} 
+            status={t.status} 
+            prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
+    
 
 export default ListaDeTarefas
