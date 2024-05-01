33ff0cb7359c8142a052f5e65ba56320ed906a37
natalete <natsalete14@gmail.com>
2024-05-01 19:38:45 -0300

    feat: criação da pagina nova tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import BarraLateral from "./containers/BarraLateral";
-import ListaDeTarefas from "./containers/ListaDeTarefas";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
+
 import EstiloGlobal, { Container } from "./styles";
 
 import  store from './store'
-import { Provider } from "react-redux";
+import Home from "./pages/Home";
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova Tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store ={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from "../../containers/BarraLateral";
+import ListaDeTarefas from "../../containers/ListaDeTarefas";
+
+const Home = () => (
+    <>
+        <BarraLateral />
+        <ListaDeTarefas />
+    </>
+)
+
+export default Home
