2fd5f80ddddc7f7d131cd24bc08fbeecc13598a9
natalete <natsalete14@gmail.com>
2024-05-02 13:00:36 -0300

    fix: ajustes finais

--- src/styles/variaveis.ts
+++ src/styles/variaveis.ts
@@ -1,7 +1,9 @@
-export default {
+const styles = {
   vermelho: '#C23616',
   verde: '#44BD32',
   amarelo: '#F0C11A',
   amarelo2: '#E1A32A',
   azulEscuro: '#2F3640',
 };
+
+export default styles;
